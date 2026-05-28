7ebff1ac7a766f0f46495a02f1a977fee01a06b0
Sourav <149921081+souravthakur99@users.noreply.github.com>
2026-05-29 00:48:07 +0530

    Implement majority element finder using HashMap

---
+++ HashMaps/MajorityElement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+        for(int num:nums){
+            if(map.containsKey(num)){
+                map.put(num,map.get(num)+1);
+            }
+            else{
+                map.put(num,1);
+            }
+            if(map.get(num)>n/2){return num;}
+        }
+        return -1;
+    }
+}
