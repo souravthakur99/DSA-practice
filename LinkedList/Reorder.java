1edca945290362290cee1dae8861649fc572b4bc
Sourav <149921081+souravthakur99@users.noreply.github.com>
2026-06-01 21:35:41 +0530

    Create Reorder.java

---
+++ LinkedList/Reorder.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode nex=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nex;
+        }
+        return prev;
+    }
+    public void reorderList(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode second=reverse(slow.next);
+        slow.next=null;
+        // reorder code
+        ListNode first=head;
+        
+        while(second !=null){
+            ListNode temp1=first.next;
+            ListNode temp2=second.next;
+            first.next=second;
+            second.next=temp1;
+            first=temp1;
+            second=temp2;
+        }
+        
+    }
+}
