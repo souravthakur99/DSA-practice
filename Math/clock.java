1ea57548b05a199be61a5f6cc0e9776f9d211f2f
Sourav <149921081+souravthakur99@users.noreply.github.com>
2026-06-18 21:40:15 +0530

    Implement method to calculate clock angle
    Given two numbers, hour and minutes, return the smaller angle (in degrees) formed between the hour and the minute hand.
    
    Answers within 10-5 of the actual value will be accepted as correct.
    
     
    
    Example 1:
    
    
    Input: hour = 12, minutes = 30
    Output: 165
    Example 2:
    
    
    Input: hour = 3, minutes = 30
    Output: 75
    Example 3:
    
    
    Input: hour = 3, minutes = 15
    Output: 7.5
     
    
    Constraints:
    
    1 <= hour <= 12
    0 <= minutes <= 59

---
+++ Math/clock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        
+        double minuteAngle = minutes * 6;
+        
+        double hourAngle = (hour % 12) * 30 + minutes * 0.5;
+        
+        double diff = Math.abs(hourAngle - minuteAngle);
+        
+        return Math.min(diff, 360 - diff);
+    }
+}
