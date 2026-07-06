5f76a5aa12285e83a91ae35f8c1a9097bc45b4fb
Sourav <149921081+souravthakur99@users.noreply.github.com>
2026-07-06 18:56:35 +0530

    Create Removeelement.java

---
+++ LinkedList/Removeelement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+    
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode temp=dummy;
+        while(temp.next!=null){
+            if(temp.next.val==val){
+                temp.next=temp.next.next;
+            }
+            else{
+            temp=temp.next;}
+        }
+        return dummy.next;
+    }
+}
