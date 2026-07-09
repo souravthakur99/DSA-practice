49cf58a9ebadcf889b5aeb49c99e49c506afbede
Sourav <149921081+souravthakur99@users.noreply.github.com>
2026-07-09 17:50:10 +0530

    Create LevelOrderReverse.java
    Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
    
     
    
    Example 1:
    
    
    Input: root = [3,9,20,null,null,15,7]
    Output: [[15,7],[9,20],[3]]
    Example 2:
    
    Input: root = [1]
    Output: [[1]]
    Example 3:
    
    Input: root = []
    Output: []
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [0, 2000].
    -1000 <= Node.val <= 1000

---
+++ Tree/LevelOrderReverse.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> list=new ArrayList<>();
+        if(root==null) return list;
+        Queue<TreeNode>q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n=q.size();
+            
+            List<Integer>l=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                TreeNode current=q.poll();
+                l.add(current.val);
+                if(current.left!=null){
+                    q.add(current.left);
+                }
+                if(current.right!=null){
+                    q.add(current.right);
+                }
+            }
+            list.add(l);
+        }
+         Collections.reverse(list);
+         return list;
+    }
+}
